687a2b7da1b75f2e7bb8ea9fd66b7768c68bb59e
ShernOC <sherlynea1994@hotmail.com>
2024-11-05 18:20:11 +0300

    post

--- Book/lie.js
+++ Book/lie.js
@@ -1,73 +1,190 @@
-//console.log(books);// This brings out all the contents we are working with. We have access to it. 
+// //console.log(books);// This brings out all the contents we are working with. We have access to it. 
 
-const init = () => {
-    // build the books card holder
+// const init = () => {
+//     // build the books card holder
     
-    //2. We will get the form element and an event listener 
-    const formInput = document.querySelector("#bookform");
-    formInput.addEventListener('submit', e => {
-        e.preventDefault();
-        getAllbooks(); // calling the fetched books
-    });
+//     //2. We will get the form element and an event listener 
+//     const formInput = document.querySelector("#bookform");
+//     formInput.addEventListener('submit', e => {
+//         e.preventDefault();
+//         getAllbooks(); // calling the fetched books
+//     });
 
-    // Initial render: will load first thing 
-    // Get data and load/render the books through the dom 
+//     // Initial render: will load first thing 
+//     // Get data and load/render the books through the dom 
 
-//Going to create a function that fetches and renders the books based on user input
+// //Going to create a function that fetches and renders the books based on user input
 
-    // Going to grab the input element 
+//     // Going to grab the input element 
 
-    function getAllbooks() {
+//     function getAllbooks() {
 
 
         
-        const inputarea = document.querySelector('.inputarea');
-        fetch(`http://localhost:3000/books/${inputarea.value.trim()}`)
-            .then(res => res.json())
-            .then(data => { 
-                let card = document.createElement('li');
-                card.className = "card";
-                // we will be using the innerHTML; // that will be displayed: 
-                card.innerHTML = `    
-                <div class = "content"> 
-                <img src = "${data.imageLink}"> 
-                <h2>Title:${data.title}</h2>
-                <p>
-               <span class = "author"> Author:${data.author}</span>
-                <br>
-               <span class = "pages"> Pages:${data.pages}</span>
-                <br>
-                <span class = "links"> 
-                  <a href="${data.link}" target="_blank">Book-link</a>  <span>
-                </p>
-                <br>
-                <button class = "buy">Buy</button>
-                </div>
-                `
-                // console.log the card to see that it will show up console.log(card)
-                const ul = document.querySelector('#list');
-                ul.appendChild(card)
-                ul.value = ""; 
-            }
+//         const inputarea = document.querySelector('.inputarea');
+//         fetch(`http://localhost:3000/books/${inputarea.value.trim()}`)
+//             .then(res => res.json())
+//             .then(data => { 
+//                 let card = document.createElement('li');
+//                 card.className = "card";
+//                 // we will be using the innerHTML; // that will be displayed: 
+//                 card.innerHTML = `    
+//                 <div class = "content"> 
+//                 <img src = "${data.imageLink}"> 
+//                 <h2>Title:${data.title}</h2>
+//                 <p>
+//                <span class = "author"> Author:${data.author}</span>
+//                 <br>
+//                <span class = "pages"> Pages:${data.pages}</span>
+//                 <br>
+//                 <span class = "links"> 
+//                   <a href="${data.link}" target="_blank">Book-link</a>  <span>
+//                 </p>
+//                 <br>
+//                 <button class = "buy">Buy</button>
+//                 </div>
+//                 `
+//                 // console.log the card to see that it will show up console.log(card)
+//                 const ul = document.querySelector('#list');
+//                 ul.appendChild(card)
+//                 ul.value = ""; 
+//             }
             
-        )    
+//         )    
         
       
-    }
+//     }
     
  
 
-    // // initial call to fetch and render books
-    // function initialize() {
-    //     // books.forEach(book=>renderOneBook(book))
-    //     // we are now going to call in getAllbooks
-    //     getAllbooks();
-    // }
+//     // // initial call to fetch and render books
+//     // function initialize() {
+//     //     // books.forEach(book=>renderOneBook(book))
+//     //     // we are now going to call in getAllbooks
+//     //     getAllbooks();
+//     // }
 
 
-    // // call the cards so that i can be displayed 
-    // initialize();
-}
+//     // // call the cards so that i can be displayed 
+//     // initialize();
+// }
 
 
-document.addEventListener('DOMContentLoaded', init)
+// document.addEventListener('DOMContentLoaded', init)
+
+// // the fetching tack tick, 
+
+// function fetchAllBooks(books){
+//     const input = document.querySelector('input');
+//             fetch(`http://localhost:3000/books/${input.value.trim()}`)
+//                 .then(res => res.json())
+//                 .then(books=>{ books.forEach(book=>renderOneBook(book))})    
+//         }
+//     }
+    
+
+
+// // The second session of the book 
+// //console.log(books);// This brings out all the contents we are working with. We have access to it. 
+
+// const init = () => {
+//     // build the books card holder
+    
+//     //2. We will get the form element and an event listener 
+//     const formInput = document.querySelector("#bookform");
+//     formInput.addEventListener('submit', handleSubmit);
+
+    
+//     const handleSubmit  = (e)=> {
+//         e.preventDefault();
+//         let books = {
+//             title:e.target.title.value,
+//             author: e.target.author.value,  
+//             pages:0,
+//             imageLink:e.target.imageLink.value,
+//         }
+
+//     // getAllbooks(books); // build the animals. 
+//     fetchAllBooks(books);
+
+//     }
+    
+
+// //Going to create a function that fetches and renders the books based on user input
+
+//     // Going to grab the input element 
+// // Build the cards. 
+//     function getAllbooks() {
+//         let card = document.createElement('li');
+//                 card.className = "card";
+//                 // we will be using the innerHTML; // that will be displayed: 
+//                 card.innerHTML = `    
+//                 <div class = "content"> 
+//                 <img src = "${data.imageLink}"> 
+//                 <h2>Title:${data.title}</h2>
+//                 <p>
+//                <span class = "author"> Author:${data.author}</span>
+//                 <br>
+//                <span class = "pages"> Pages:${data.pages}</span>
+//                 <br>
+//                 <span class = "links"> 
+//                   <a href="${data.link}" target="_blank">Book-link</a>  <span>
+//                 </p>
+//                 <br>
+//                 <button class = "buy">Buy</button>
+//                 </div>
+//                 `
+//                 // console.log the card to see that it will show up console.log(card)
+//                 const ul = document.querySelector('#list');
+//                 ul.appendChild(card)
+//                 ul.value = ""; 
+//             }
+
+// function fetchAllBooks(){
+//     fetch("http://localhost:3000/books")
+//     .then(res => res.json())
+//     .then(books=>{ books.forEach(book=>getAllbooks(book))})    
+//     }
+// }
+
+// // function fetchOneBook(){
+// //     fetch()
+// // }
+
+    
+    
+// //     // initial call to fetch and render books
+//     function initialize() {
+//         // books.forEach(book=>renderOneBook(book))
+//         // we are now going to call in getAllbooks
+//        getAllbooks();
+//     }
+
+//     // call the cards so that i can be displayed 
+//     initialize();
+
+
+// document.addEventListener('DOMContentLoaded', init)
+
+
+
+// // const init = () => {
+// //     const inputForm = document.querySelector("form");
+  
+// //     inputForm.addEventListener("submit", (event) => {
+// //       event.preventDefault();
+// //       const input = document.querySelector("input#searchByID");
+  
+// //       fetch(`http://localhost:3000/movies/${input.value}`)
+// //         .then((response) => response.json())
+// //         .then((data) => {
+// //           const title = document.querySelector("section#movieDetails h4");
+// //           const summary = document.querySelector("section#movieDetails p");
+  
+// //           title.innerText = data.title;
+// //           summary.innerText = data.summary;
+// //         });
+// //     });
+// //   };
+  
+// //   document.addEventListener("DOMContentLoaded", init);
--- Book/small.js
+++ Book/small.js
@@ -1,72 +1,86 @@
-//console.log(books);// This brings out all the contents we are working with. We have access to it. 
-
 const init = () => {
-    // build the books card holder
-    
-    //2. We will get the form element and an event listener 
-    const formInput = document.querySelector("#bookform");
-    formInput.addEventListener('submit', e => {
-        e.preventDefault();
-        let books = {
-            title:e.target.title.value,
-            imageLink:e.target.imageLink.value,
-            pages:e.target.pages.value,
-        }
-
-        getAllbooks(books); // calling the fetched books
-    });
-
-    // Initial render: will load first thing 
-    // Get data and load/render the books through the dom 
-
-//Going to create a function that fetches and renders the books based on user input
-
-    // Going to grab the input element 
-
-    function getAllbooks(books) {
+    //GET-RETRIEVE DATA 
+    // card holding 
+    function holdTheCard(book) {
         let card = document.createElement('li');
-                card.className = "card";
-                // we will be using the innerHTML; // that will be displayed: 
-                card.innerHTML = `    
-                <div class = "content"> 
-                <img src = "${data.imageLink}"> 
-                <h2>Title:${data.title}</h2>
-                <p>
-               <span class = "author"> Author:${data.author}</span>
-                <br>
-               <span class = "pages"> Pages:${data.pages}</span>
-                <br>
-                <span class = "links"> 
-                  <a href="${data.link}" target="_blank">Book-link</a>  <span>
-                </p>
-                <br>
-                <button class = "buy">Buy</button>
-                </div>
-                `
-                // console.log the card to see that it will show up console.log(card)
-                const ul = document.querySelector('#list');
-                ul.appendChild(card)
-                ul.value = ""; 
-            }
+        card.className = "card";
+        card.innerHTML = `
+        <div class = "content"> 
+        <img src = "${book.imagelink}">
+        <h2>Title:${book.title}</h2>
+         <p>
+         <span class = "author"> Author:${book.author}</span>
+         <br>
+         <span class = "pages"> Pages:${book.pages}</span>
+         <br>
+         <span class = "links"> 
+          <a href="${book.link}" target="_blank">Book-link</a>  <span>
+           </p>
+           <br>
+           <button class = "buy">Buy</button>
+           </div>
+            `
+        // append the li to the ul element
+        const ul = document.querySelector('#list');
+        ul.appendChild(card)
+    };
 
+// created a function that fetches all the books
+function fetchAllBooks(){
+    fetch("http://localhost:3000/books")
+    .then(res=>res.json())
+    .then(books =>{
+        books.forEach(book=>holdTheCard(book))
+    })
+};
 
-        // fetch request 
-        const inputarea = document.querySelector('.inputarea');
-        fetch(`http://localhost:3000/books/${inputarea.value.trim()}`)
-            .then(res => res.json())
-            .then(books=>{ books.forEach(book=>renderOneBook(book))})    
+//POST 
+form = document.querySelector('#bookform');
+form.addEventListener('submit', handleSubmit); 
+
+// create a form event handler object
+function handleSubmit(e){
+    e.preventDefault();
+    let books = {
+        title:e.target.title.value,
+        author:e.target.author.value,
+        imagelink:e.target.imagelink.value,
+        pages:23
     }
-    
-    // initial call to fetch and render books
-    function initialize() {
-        // books.forEach(book=>renderOneBook(book))
-        // we are now going to call in getAllbooks
-        getAllbooks();
-    }
+// pass in the function that holds holds all the cards 
+holdTheCard(books);
+fetchOneBook(books);
+}
+
+// create a function that fetches the input of a user 
+function fetchOneBook(){
+    fetch('http://localhost:3000/books',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(books)
+    })
+    .then(res=>res.json())
+    .then(book=>console.log(book))
+}
 
 
-    // call the cards so that i can be displayed 
-    initialize();
+// console.log(holdTheCard());
+
+// initial run of the code
+// call the cards so that i can be displayed 
+function runTheCode() {
+  fetchAllBooks();
+}
+
+runTheCode();
+
+}
 
 
+
+console.log("life is great"); 
+
+// DOM manipulation: 
 document.addEventListener('DOMContentLoaded', init)
