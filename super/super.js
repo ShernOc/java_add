e5f523880aeb348c6531dd1f301ba93a8ef7fb86
ShernOC <sherlynea1994@hotmail.com>
2025-04-18 11:35:27 +0300

    add super folder

---
+++ super/super.js
@@ -0,0 +1,32 @@
+//Variables: container that holds, and gives and holds data.  
+
+let variables1 =3;
+console.log(variables1);
+
+const word = "Amazing how you forgot shit";
+console.log(word,variables1);
+1
+// resign a variable*
+mine1 = 10;
+mine1 = mine1 +1;
+console.log(mine1)
+
+// Start Quantity = 0 
+let Quantity = 0; 
+
+// add the value of Quantity to two and then reasign it to that quantity
+Quantity = Quantity +2; 
+
+Quantity = Quantity +3;  
+
+
+
+// string interpolation the use of back ticks.
+/*to make the quantity to be seen we have to do use a backticks for the work */
+// string interpolation 
+console.log(`Cart quantity:${Quantity}`)
+
+
+
+
+
