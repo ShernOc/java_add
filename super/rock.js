228bbd39bdbb7f46fbfc68758ce84ccd6af28f07
ShernOC <sherlynea1994@hotmail.com>
2025-04-29 13:46:58 +0300

    update

---
+++ super/rock.js
@@ -0,0 +1,19 @@
+const names = "sherlyne "
+const randomNumber = Math.floor(Math.random());
+
+
+// function life(randomNumber){
+  
+//   if (randomNumber >= 0 && randomNumber<3){
+//   console.log(`rock`)}
+//   else if (randomNumber>=3 && randomNumber<6 ){
+//   console.log(`paper`);
+//   } 
+//   else if(randomNumber>= 6 && randomNumber<9){
+//    console.log(`scissor`);
+//   }
+// }
+   
+console.log(names)
+
+
