3b381b1e0bcec79540010373ef873f2ec185636d
ShernOC <sherlynea1994@hotmail.com>
2025-09-18 21:26:09 +0300

    update the quantity so it's calculated

--- super/Amazon/amazon.js
+++ super/Amazon/amazon.js
@@ -9,50 +9,76 @@ const shipping_cost = 499;
 const tax = 0.1
 
 //Variable for Quantity 
-let cartQuantity = 0;
+let cartQuantity =0;
 
 // Function for top section: 
 // Display items: 
- function displayItems(){
+
+ function totalItems(){
     const basket_span = document.querySelector('.js-basket-span').innerText = ` Price: $ ${basket/100}`;
     const shoe_span = document.querySelector('.js-shoe-span').innerText = ` Price $ ${shoe/100}`;
     const toaster_span = document.querySelector('.js-toaster-span').innerText =` Price $ ${toaster/100}`;
     const t_shirt_span = document.querySelector('.js-shirt-span').innerText = `Price $ ${t_shirt/100}`;
 
-    const bt = document.querySelector('.js-input').innerHTML = `${console.log(cartQuantity)}`
-
-    return basket_span;
-
+    return shoe_span;
  }
 
- console.log(displayItems())
+ console.log(totalItems())
     
 // I want to display the quantity that have pressed on the button  * the price of the item  
 // once an item is selected it now able be added as an item.
 
 
-function amazonCalculation(){
-    //How to add the quantity to the application. 
-    // Calculate the quantity
-    const mes = cartQuantity * shoe
-    console.log(mes)
-   
-    let shoe_item = 2*shoe; 
-    let bask_item = 2*basket;
-    //Array of items
-    const items_array = [bask_item,bask_item,toaster,t_shirt,shoe_item]
-    
-    //Total cost shoe + basket
-    const items = items_array.reduce((a,b)=>a+b,0) 
-    console.log(items)//4275
+//BUTTONS CALCULATION 
+function displayQuantity(){
+    const item = document.querySelector('.js-show-quantity').innerText = `Cart quantity: ${cartQuantity}`;
+    return item;
+}
+console.log(displayQuantity())
 
-    // Show items in an array 
-    const size = items_array.length 
-    console.log(size)
+
+const maxItems = 10; // maximum items in the cart
+// times (number of times (+1,-1,) its a parameter that hols +1,-2, ...)
+function amazonCart(times){
+    displayQuantity();// updates the quantity in the paragraph, 
+    if(cartQuantity + times > maxItems){
+        return alert('Cart is full maximum items = 10')
+    }else if (cartQuantity + times < 0){
+         alert("Cart cannot be zero");
+        return cartQuantity;
+    }
+    
+    cartQuantity += times ; 
+    amazonCalculation(); // the total calculations or quantities
+    return cartQuantity;
+}
+
+console.log(amazonCart(0))
+
+// Take the number of quantity in the amazon cart, 
+function amazonCalculation(){
+    // Pass the amazon cart that updates the items; 
+    // Calculate the quantity
+    const quantity = cartQuantity;
+    console.log(quantity) // 
+   
+    // calculate the items * amount of the item 
+    let shoe_item =quantity *shoe; 
+    console.log(shoe_item);
+
+    // //Array of items
+    // const items_array = [shoe_item] 
+    // //Total cost shoe + basket
+    // const items = items_array.reduce((a,b)=>a+b,0) 
+    // console.log(items)//4275
+
+    // // Show items in an array 
+    // const size = items_array.length 
+    // console.log(size)
 
 
 // Total + shipping 
-    const t_shipping = (items + shipping_cost)/100;
+    const t_shipping = (shoe_item + shipping_cost)/100;
     console.log(t_shipping)//4774 
     
     // tax 
@@ -64,49 +90,24 @@ function amazonCalculation(){
     console.log(total_payment) //5251.3
 
 
-//JS DISPLAY IN THE CONSOLE 
+//JS DISPLAY IN THE DOM  
      // how to pass the items of a functions to the current function: 
     //totalItem(); 
-    const size_items = document.querySelector('.js-items').innerHTML = `Items (${size}): $${items/100}`;
+    const size_items = document.querySelector('.js-items').innerHTML = `Items (${quantity}): $${shoe_item/100}`;
     const ship = document.querySelector('.js-ship').innerText = `Shipping & handling: $${shipping_cost/100}`;
     const b_cost = document.querySelector('.js-before-tax').innerText = `Total before tax: $${t_shipping}`;
     const taxe = document.querySelector('.js-tax').innerHTML = `Estimated tax(10%): $${total_tax}`; 
     const total_cost = document.querySelector('.js-total').innerHTML =`Order total: $${total_payment}`;
 
-    return size_items,ship,b_cost,taxe,total_cost,total_payment ;// 52.51
+    return total_payment ;// 52.51
 }
 
 console.log(amazonCalculation())
 
 
-//SHOW QUANTITY
-
-/*add to cart = (+=1);
-+2 
-remove from cart = (-=1)
-*/
-// console.log(amazonCart());
 
 
-function displayQuantity(){
-    const item = document.querySelector('.js-show-quantity').innerText = `Cart quantity: ${cartQuantity}`;
-     return item
-}
 
-console.log(displayItems())
-
-const maxItems = 10; // maximum items in the cart
-// times (number of times (+1,-1,) its a parameter that hols +1,-2, ...)
-function amazonCart(times){
-    displayQuantity() // updates the quantity in the paragraph, 
-    if(cartQuantity + times > maxItems){
-        return alert('Cart is full maximum items = 10')
-    }else if (cartQuantity + times < 0){
-        return alert("Cart cannot be zero")
-    }
-
-    return cartQuantity +=times ; 
-}
 
 // console.log(displayQuantity(amazonCart(times)))
 
--- super/lessons.js
+++ super/lessons.js
@@ -96,6 +96,71 @@ console.log(amazonCart(times))
 
 
 
+//Amazon Function
+function amazonCalculation(){
+    // Pass the display Quantity; 
+    displayItems();
+    //How to add the quantity to the application. 
+    // Calculate the quantity
+    const mes = displayItems() * shoe
+    console.log(mes)
+   
+    let shoe_item = 2*shoe; 
+    let bask_item = 2*basket;
+
+    
+    //Array of items
+    const items_array = [bask_item,bask_item,toaster,t_shirt,shoe_item]
+    
+    //Total cost shoe + basket
+    const items = items_array.reduce((a,b)=>a+b,0) 
+    console.log(items)//4275
+
+    // Show items in an array 
+    const size = items_array.length 
+    console.log(size)
+
+
+// Total + shipping 
+    const t_shipping = (items + shipping_cost)/100;
+    console.log(t_shipping)//4774 
+    
+    // tax 
+    const total_tax = (t_shipping * tax) ;
+    console.log(total_tax)// 477.4000 
+
+    // Total payment 
+    const total_payment = (Math.round(t_shipping + total_tax));
+    console.log(total_payment) //5251.3
+
+
+//JS DISPLAY IN THE CONSOLE 
+     // how to pass the items of a functions to the current function: 
+    //totalItem(); 
+    const size_items = document.querySelector('.js-items').innerHTML = `Items (${size}): $${items/100}`;
+    const ship = document.querySelector('.js-ship').innerText = `Shipping & handling: $${shipping_cost/100}`;
+    const b_cost = document.querySelector('.js-before-tax').innerText = `Total before tax: $${t_shipping}`;
+    const taxe = document.querySelector('.js-tax').innerHTML = `Estimated tax(10%): $${total_tax}`; 
+    const total_cost = document.querySelector('.js-total').innerHTML =`Order total: $${total_payment}`;
+
+    return size_items,ship,b_cost,taxe,total_cost,total_payment ;// 52.51
+}
+
+console.log(amazonCalculation(displayItems))
+
+
+//SHOW QUANTITY
+
+/*add to cart = (+=1);
++2 
+remove from cart = (-=1)
+*/
+// console.log(amazonCart());
+
+
+
+
+
 
 
 
